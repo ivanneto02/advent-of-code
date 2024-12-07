d3e583736223429c3d983168314fa89c386e8a58
Ivan Neto <ivan.a.neto@hotmail.com>
2024-12-07 00:38:34 -0800

    Pre-adding files for day8

---
+++ day8/main.py
@@ -0,0 +1,16 @@
+
+def main():
+    
+    print("Part1: ", part1())
+    print("Part2: ", part2())
+
+def part1():
+
+    return 0
+
+def part2():
+
+    return 0
+
+if __name__ == "__main__":
+    main()
