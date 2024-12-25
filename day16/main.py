fd713a016cfdfbf04e523719d2ad551585c5fb51
ivanneto02 <ivan.a.neto@hotmail.com>
2024-12-25 01:11:17 -0800

    Adding day16

--- day16/main.py
+++ day16/main.py
@@ -38,7 +38,7 @@ def h(curr, end):
 def f(gmap, curr, end):
     return g(gmap, curr) + h(curr, end)
 
-def make_path(from_map, curr):
+def make_path(from_map, curr, start):
     path = []
     while curr != start:
         path.append(curr)
@@ -46,26 +46,31 @@ def make_path(from_map, curr):
     path.append(start)
     return path[::-1] # reverse
 
-"""
-We are assuming a standard coordinate system, where we increase x and y as we move right and up
-We also start at (1,1) because of the border.
-"""
+# For debugging, returns a state with every point in the path replaced with +
+def traversed_state(state, path):
+    for point in path:
+        x, y = point
+        state[y][x] = "+"
+    return state
+
 def AStar(state, start, goal):
 
-    gmap = []
+    gmap = {}
     explored = {}
     from_map = {}
     frontier = []
 
-    heapq.heappush(frontier, (0, (1, 1)))
+    heapq.heappush(frontier, (0, start))
 
     gmap[start] = 0
+    direction = "E"
+    last = start
 
     while frontier:
 
         # get lowest cost node, remove from frontier
-        curr = heapq.heappop(frontier)
-
+        curr = heapq.heappop(frontier)[1]
+    
         explored[curr] = True
 
         if curr == goal:
@@ -76,39 +81,49 @@ def AStar(state, start, goal):
 
         # explore neighbors if possible
         explore_east  = state[y][x-1] != "#" and (x-1, y) not in explored
-        explore_north = state[y+1][x] != "#" and (x, y+1) not in explored
+        explore_north = state[y-1][x] != "#" and (x, y-1) not in explored
         explore_west  = state[y][x+1] != "#" and (x+1, y) not in explored
         explore_south = state[y-1][x] != "#" and (x, y-1) not in explored
+    
+        # check direction based on where curr is coming from!
        
         if explore_east:
             toScore = gmap[curr] + 1
+            if direction == "N" or direction == "S":
+                toScore += 1000
             if (x-1, y) not in gmap or toScore < gmap[(x-1, y)]: # found more optimal path
                 from_map[(x-1, y)] = curr
                 gmap[(x-1, y)] = toScore
-                heapq.heappush((f(gmap, curr, goal) , (x-1, y)))
+                heapq.heappush(frontier, (f(gmap, (x-1,y), goal) , (x-1, y)))
 
         if explore_north:
             toScore = gmap[curr] + 1
-            if (x, y+1) not in gmap or toScore < gmap[(x, y+1)]: # found more optimal path
-                from_map[(x, y+1)] = curr
-                gmap[(x, y+1)] = toScore
-                heapq.heappush((f(gmap, curr, goal) , (x, y+1)))
-
-        if explore_west:
-            toScore = gmap[curr] + 1
-            if (x+1, y) not in gmap or toScore < gmap[(x+1, y)]: # found more optimal path
-                from_map[(x+1, y)] = curr
-                gmap[(x+1, y)] = toScore
-                heapq.heappush((f(gmap, curr, goal) , (x+1, y)))
-
-        if explore_south:
-            toScore = gmap[curr] + 1
+            if direction == "E" or direction == "W":
+                toScore += 1000
             if (x, y-1) not in gmap or toScore < gmap[(x, y-1)]: # found more optimal path
                 from_map[(x, y-1)] = curr
                 gmap[(x, y-1)] = toScore
-                heapq.heappush((f(gmap, curr, goal) , (x, y-1)))
+                heapq.heappush(frontier, (f(gmap, (x, y-1), goal) , (x, y-1)))
 
-def get_stard_end_pos(state, width, height):
+        if explore_west:
+            toScore = gmap[curr] + 1
+            if direction == "N" or direction == "S":
+                toScore += 1000
+            if (x+1, y) not in gmap or toScore < gmap[(x+1, y)]: # found more optimal path
+                from_map[(x+1, y)] = curr
+                gmap[(x+1, y)] = toScore
+                heapq.heappush(frontier, (f(gmap, (x+1, y), goal) , (x+1, y)))
+
+        if explore_south:
+            toScore = gmap[curr] + 1
+            if direction == "E" or direction == "W":
+                toScore += 1000
+            if (x, y+1) not in gmap or toScore < gmap[(x, y+1)]: # found more optimal path
+                from_map[(x, y+1)] = curr
+                gmap[(x, y+1)] = toScore
+                heapq.heappush(frontier, (f(gmap, (x, y+1), goal) , (x, y+1)))
+    
+def get_start_end_pos(state, width, height):
     spos = (0, 0)
     epos = (0, 0)
     for j in range(height):
@@ -126,11 +141,22 @@ number_of_moves + 1000*number_of_turns
 @timing
 def part1(fname):
     
-    f = open(fname).read()
+    f = open(fname).read().strip()
 
-    
+    state = [ list(x) for x in f.split("\n") ]
+    width = len(state[0])
+    height = len(state)
 
+    start, goal = get_start_end_pos(state, width, height)
+
+    path = AStar(state, start, goal)
     
+    traversed = traversed_state(state, path)
+    
+    print("\n")
+    for s in state:
+        print("".join(s))
+
     return 0
 
 @timing
